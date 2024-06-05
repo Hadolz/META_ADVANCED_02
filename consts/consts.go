6d05d219f9120e50a3149b46835156b6dfa9445d
Hadolz <moyosore815@gmail.com>
2024-06-05 22:15:57 +0100

    Update consts.go

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = "JOEY"
+	HRP = "HAD"
 	// TODO: choose a name for your hyperchain
-	Name = "JOEYSVM"
+	Name = "HADOLS"
 	// TODO: choose a token symbol
-	Symbol = "JOE"
+	Symbol = "HD"
 )
 
 var ID ids.ID
